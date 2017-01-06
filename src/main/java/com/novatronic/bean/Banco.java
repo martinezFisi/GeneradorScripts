a8cc6a7f42518f9778487dfca30bdd4f4c22ee5f
Antony Martinez Mora <amartinez@novatronic.com>
2017-01-06 15:26:42 -0500

    primer commit

---
+++ src/main/java/com/novatronic/bean/Banco.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.novatronic.bean;
+
+/**
+ *
+ * @author amartinez
+ */
+public class Banco {
+    
+}
---
+++ src/main/java/com/novatronic/generadorscripts/App.java
@@ -0,0 +1,13 @@
+package com.novatronic.generadorscripts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
